f01e314f688c0160bdd734e2c5637e20515d2488
Mayrom <mayrom.guilherme@outlook.com>
2024-11-14 21:20:26 -0300

    created HomeActivity

--- app/src/main/java/com/example/recoleta/HomeActivity.java
+++ app/src/main/java/com/example/recoleta/HomeActivity.java
@@ -1,6 +1,12 @@
 package com.example.recoleta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private ImageView profileImgView, altImgView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //recuperando os dados
+        UserDataSession userData = new UserDataSession(this);
+
+        profileImgView = (ImageView) findViewById(R.id.imageView2);
+        altImgView = (ImageView) findViewById(R.id.imageView3);
+
+        profileImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(HomeActivity.this,
+                        "Bem-Vindo, " + userData.getFirstName(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        altImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, UserActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
